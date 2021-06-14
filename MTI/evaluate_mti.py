d24a5e8cdd11d49a1f27495f6db6f3a9d284910a
terry07 <07stamatis07@gmail.com>
2021-06-14 10:56:08 +0300

    update files after review vol2

--- MTI/evaluate_mti.py
+++ MTI/evaluate_mti.py
@@ -2,61 +2,70 @@
 """
 Created on Wed Jun  9 18:05:01 2021
 
-@author: stam
+@authors:
+Nikos Mylonas   myloniko@csd.auth.gr
+Stamatis Karlos stkarlos@csd.auth.gr
+Grigorios Tsoumakas greg@csd.auth.gr
 """
 
 import os, pickle
-os.chdir(r'C:\Users\stam\Documents\git\Instance-Based-ZSL\Results\imperfect oracle')
-os.chdir(r'C:\Users\stam\Documents\git\Instance-Based-ZSL\pre-computed files')
+path = r'C:\Users\stama\Documents\github\\'
 
 
-z = 'noisy_labels_70percent.pickle'
+choice = input('Give your choice regarding the set of predicted existing labels that you want to evaluate: \n1. MTI \n2. Noisy labels (random choice) \n\n ...')
+
+if choice == '1':
+
+	os.chdir(path + '\Instance-Based-ZSL\MTI')
+	z = 'mti_predictions.pickle'
+
+	with open(z, "rb") as f:
+	    		y_preds = pickle.load(f)
+	f.close() 
+
+
+elif choice == '2':
+   
+   # user can provide any other .pickle file that contains predictions of the existing labels per test instance and obtain the corresponding metrics, as in the case of MTI
+	#os.chdir(path + '\Instance-Based-ZSL\pre-computed files')
+	#z = 'noisy_labels_.pickle' 
+
+	with open(z, "rb") as f:
+	    		y_preds = pickle.load(f)
+	f.close()
+
+else:
+	raise SystemExit('Wrong input')
+
+# load the actual labels
+os.chdir(path + '\Instance-Based-ZSL\pre-computed files')
 z = 'known_labels.pickle'
 
 with open(z, "rb") as f:
-    			y = pickle.load(f)
+    		y_actual = pickle.load(f)
 f.close() 
 
-os.chdir(r'C:\Users\stam\Documents\git\Instance-Based-ZSL\MTI')
 
-z = 'mti_predictions.pickle'
-
-with open(z, "rb") as f:
-    			y_mti = pickle.load(f)
-f.close() 
-
+#%%
 
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import MultiLabelBinarizer
 
-y_true = [(12, 2, 4, 5),
-          (5, 2),
-          (12,)
-         ]
-
-y_pred = [(4, 5),
-          (5, 2),
-          (5, 4)
-         ]
-
 mlb = MultiLabelBinarizer()
 
-y_true_binarized = mlb.fit_transform(y_true)
-y_pred_binarized = mlb.transform(y_pred)
-
-print(classification_report(y_true_binarized, y_pred_binarized, 
-                            target_names=[str(cls) for cls in mlb.classes_]))
+y_true_binarized = mlb.fit_transform(y_actual)
+y_pred_binarized = mlb.transform(y_preds)
 
 
-
-y_true_binarized = mlb.fit_transform(y)
-y_pred_binarized = mlb.transform(y_mti)
-
-
+# Examine the accuracy of the existing labels that are used for creating the proposed multi-label ranking approach
 print(classification_report(y_true_binarized, y_pred_binarized))
-
 
 #   micro avg       0.63      0.58      0.60    569994
 #   macro avg       0.59      0.61      0.56    569994
 #weighted avg       0.67      0.58      0.58    569994
 # samples avg       0.63      0.59      0.59    569994
+
+
+# for specific labels or for all the existing ones
+#print(classification_report(y_true_binarized, y_pred_binarized, 
+#                           target_names=[str(cls) for cls in mlb.classes_]))
